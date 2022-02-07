cfdec4f8eb96ca65e962c2b608fd6f408fffef82
Sergey_Grishin <svg95@list.ru>
2022-02-07 13:59:46 +0300

    Task_2 has been changed

--- lesson_2/Task_2.py
+++ lesson_2/Task_2.py
@@ -1,27 +1,28 @@
-list_1=['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+#list_1=['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 list_2=['примерно в', '23', 'часа', '8', 'минут', '03', 'секунд', 'температура', 'воздуха', 'была', '-5', 'градусов Цельсия', 'темп', 'воды', '+12', 'градусов', 'Цельсия']
-lists=[list_1, list_2]
+lists=[list_2]
 for lst in lists:
     len_list = len(lst)
     i=0
-    while i!=len_list+i//3: # я не знал как сделать этот цикл, поэтому методом тыка +i//3 =)
+    print("Исходный список:", '\n',lst,'\n')
+    while i!=len_list: # я не знал как сделать этот цикл, поэтому методом тыка +i//3 =)
         if lst[i].isdigit()==True:
-            lst[i]=list_1[i].zfill(2)
+            lst[i]=lst[i].zfill(2)
             lst.insert(i,'"')
             lst.insert(i+2,'"')
-            i+=2
-        elif '+'in lst[i]:
-            _,v1=lst[i].split('+')
-            lst[i] ="+%s" %(v1.zfill(2))
-            lst.insert(i,'"')
-            lst.insert(i+2,'"')
-            i+=2
-        elif '-' in lst[i]:
-            _, v1 = lst[i].split('-')
-            lst[i]="-%s" % (v1.zfill(2))
-            lst.insert(i,'"')
-            lst.insert(i+2,'"')
-            i+=2
+            i+=3
+        if '-'in lst[i] or '+'in lst[i]:
+            lst[i] = lst[i].zfill(3)
+            lst.insert(i, '"')
+            lst.insert(i + 2, '"')
+            i += 3
+        len_list = len(lst)
+        i+=1
+    for x in range(len_list):
+        if lst[x].isdigit()== True:
+            list_str =''.join(lst)
         else:
-            i+=1
-    print(lst,'\n')
+            list_str =' '.join(lst)
+#        list_str=list_str.replace(' " ','"')
+    print('Новый список + добавление элементов-кавычек:','\n',lst,'\n','\n','Строка:','\n', list_str,'\n','\n','\n','====================================================')
+
