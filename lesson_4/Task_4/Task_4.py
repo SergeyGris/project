144221b006b5e44685254e3849ce6c4944c22a26
Sergey_Grishin <svg95@list.ru>
2022-02-12 22:22:17 +0300

    lesson_4

--- lesson_4/Task_4/Task_4.py
+++
@@ -1,7 +0,0 @@
-from utils import currency_rates
-
-url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-currency_rates(url, 'USd')
-currency_rates(url, "EuR")
-currency_rates(url, "GBP")
-currency_rates(url, "GBP2")
